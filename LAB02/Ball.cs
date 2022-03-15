67cd6685db1b11aff2bace0ca4aa2ea69006ff25
Tahoor Binte Ali <tahoorali908@gmail.com>
2022-03-15 23:20:45 +0500

    Update Ball.cs

--- LAB02/Ball.cs
+++ LAB02/Ball.cs
@@ -22,6 +22,7 @@ public class Ball : MonoBehaviour
             ball.velocity = ball.velocity * ballSpeedIncrement;
             increasePaddleSpeed();
         }
+        // Implemented by Tahoor
         else if (collision.collider.CompareTag("LeftWall"))
         {
             updatePlayerScore("RightPlayerScore", ++GameManager.GetGameManager().rightPlayerScore);
@@ -40,7 +41,7 @@ public class Ball : MonoBehaviour
             paddle.Speed += paddleSpeedIncrement;
         }
     }
-
+// Implemented by Tahoor
     void updatePlayerScore(string tag, int score)
     {
         GameObject.FindGameObjectWithTag(tag).GetComponent<Text>().text = score.ToString();
