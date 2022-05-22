11f28b1574930cbcd6bc799a758f1d66679043ed
Abdul Moiz Ali <moizali_@outlook.com>
2022-05-22 22:29:21 +0500

    Add files via upload

---
+++ Final/Platform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            Invoke("FallDown", 0.1f);
+        }
+    }
+
+    private void FallDown()
+    {
+        this.GetComponentInParent<Rigidbody>().isKinematic = false;
+        Destroy(this.transform.parent.gameObject, 2f);
+    }
+}
